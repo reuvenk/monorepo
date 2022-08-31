068746f0bd08394efb18f274707b24a0639abb81
Ruven Kadison <reuven.kadison@sap.com>
2022-08-31 14:32:38 +0300

    segment-recalc added

---
+++ services/segment-recalc/src/main/java/com/gigya/cdp/App.java
@@ -0,0 +1,21 @@
+package com.gigya.cdp;
+
+import com.gigya.cdp.common.CdpLogger;
+import com.gigya.cdp.profilecombiner.api.IProfileCombiner;
+import com.gigya.cdp.profilecombiner.logic.ProfileCombiner;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    static IProfileCombiner profileCombiner = new ProfileCombiner();
+    static CdpLogger logger = new CdpLogger();
+    public static void main( String[] args )
+    {
+        logger.log("Starting Segment - Recalc");
+
+        profileCombiner.HandleProfileUpdate("from  Segment - Recalc");
+    }
+}
